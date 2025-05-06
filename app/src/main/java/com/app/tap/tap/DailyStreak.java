3780e6f1971cf389a2e979523827f64d2f5f5018
Ayush Raj <parmod.k.201555@gmail.com>
2025-05-07 00:23:22 +0530

    payment | daily rewards

---
+++ app/src/main/java/com/app/tap/tap/DailyStreak.java
@@ -0,0 +1,16 @@
+package com.app.tap.tap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import com.example.taptap.R;
+
+public class DailyStreak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_streak);
+    }
+}
